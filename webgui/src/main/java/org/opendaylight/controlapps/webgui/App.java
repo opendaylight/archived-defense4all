561ecc5f9b4893104db69645f9f60805f1d52e53
gerag <gerag@radware.com>
2013-08-04 14:39:25 +0300

    Change-Id: I86a5fc8c79ecb43ed83201f107c25301acd64615 Signed-off-by: gerag <gerag@radware.com>

--- webgui/src/main/java/org/opendaylight/controlapps/webgui/App.java
+++ webgui/src/main/java/org/opendaylight/controlapps/webgui/App.java
@@ -19,7 +19,7 @@ import com.sun.jersey.api.client.WebResource;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  
 
 		Client client = Client.create();
 		
